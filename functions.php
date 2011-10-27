b47fc7b31ae87a1d51d2fc58e5c594325acce9c6
Pavel Topinka <ptopinka@gmail.com>
2011-10-27 15:35:30 +0200

    prvni verze webu

---
+++ functions.php
@@ -0,0 +1,142 @@
+﻿<?php
+
+function printMeta() {
+	print <<<END
+		 <META HTTP-EQUIV="Pragma" CONTENT="no-cache"> 
+
+		<META HTTP-EQUIV="Expires" CONTENT="-1"> 
+		<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE"> 
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
+END;
+}
+
+
+function printHeader() {
+	print <<<END
+  		 <div id="header">
+          <div class="headerlogo">
+  	    	  <a class="noborder1" href="index.php" alt="Svatebni dům Zbraslav" title="Svatební dům Zbraslav"><img src="img/web_zahlavi_logo.png"></a> 
+          </div>
+		      <div class="headerbanner">
+		        <a class="noborder" href="index.php" alt="Svatebni dům Zbraslav" title="Svatební dům Zbraslav"><img src="img/header.png"></a>
+          </div>
+
+          
+			    <div id="prouzek"> </div></div>
+			</div>
+END;
+}
+function printMenu() {
+	print <<<END
+    menu sloupec
+    <div id="sidetree">
+
+
+    </div>
+
+				
+END;
+}
+
+
+
+function printTitle() {
+	print <<<END
+  	<title>Svatební dům Zbraslav</title>
+END;
+}
+
+
+
+function printRightBanner() {
+
+  print <<<END
+    <div class="banner">
+   		<a>
+   						<img src="img/Pruhonice.png"
+   								 height="200" width="200" />
+       </a>
+             Svatební festival v Průhonicích je skvělá akce kde si můžete prohlédnout vybrané kolekce našich předních návrhářů.
+     </div>
+END;
+}
+
+
+
+function printFooter() {
+	print <<<END
+      <p>Veroo (r)</p>
+END;
+}
+
+
+function includeCSS_JS_noLeftMenu() {
+	print <<<END
+
+    <link href="css/layout_noleftmenu.css" rel="stylesheet">
+
+
+    <script src="js/jquery/jquery.js"></script>
+    <script src="js/jquery/jquery.tpl_layout.js"></script>
+END;
+} 
+
+function includeCSS_JS() {
+	print <<<END
+
+<link href="css/layout.css" rel="stylesheet">
+<link href="css/global.css" rel="stylesheet">
+
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js"></script>
+<script src="js/jquery/jquery.tpl_layout.js"></script>
+<script src="js/jquery/jquery.cookie.js" type="text/javascript"></script>
+<script src="js/jquery/jquery.treeview.js" type="text/javascript"></script>
+
+<link rel="stylesheet" href="js/jquery/jquery.treeview.css" />
+<link rel="stylesheet" href="js/jquery/red-treeview.css" />
+<link rel="stylesheet" href="js/jquery/screen.css" />
+
+  <script type="text/javascript" src="js/highslide/highslide.js"></script>		<!--FOTO ZOOM -->
+<link rel="stylesheet" type="text/css" href="js/highslide/highslide.css" />		<!--FOTO ZOOM -->
+  
+
+
+
+<script>
+$.templateLayoutShowOnReady();
+
+	
+	$(function() {
+		$.setTemplateLayout('css/layout.css');
+	});
+	</script>
+	
+<script type="text/javascript">													<!--FOTO ZOOM -->
+    hs.graphicsDir = 'js/highslide/graphics/';
+    hs.outlineType = 'rounded-white';
+</script>
+
+  <script type="text/javascript">
+       $(function(){
+
+        $("#tree").treeview({
+         collapsed: true,
+   				animated: "medium",
+   				persist: "location"
+        });
+
+        //$("#tree").treeview({collapsed:true})
+
+       });
+
+   	</script>
+
+
+END;
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,94 @@
+﻿<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+ 
+<?php 
+  require("functions.php");
+ ?>
+
+<head>
+		<?php
+		printMeta () ;
+		?>
+  <?php
+    printTitle();
+  
+    includeCSS_JS();
+  ?>
+
+
+</head>
+
+<body>
+
+	
+		<?php
+		  printHeader();
+		?>
+	
+	
+	<div id="vataheader">
+	</div>
+	<div id="wataheader">
+	</div>
+	<div id="vatabody">
+	</div>
+	<div id="watabody">
+	</div>
+	<div id="vatafoot">
+	</div>
+	<div id="watafoot">
+	</div>
+	
+	<div id="pmenu">
+		
+				<?php
+				
+				printMenu();
+				?>
+				
+	</div>
+	
+		<div id="content">
+      <p>
+        Dohodu o odmazání poloviny řeckého dluhu nepovažuje ministr financí Miroslav Kalousek za definitivní řešení. Klíčové je podle nich to, zda se podaří posílit evropské banky. "Pro řadu vlád to může být nepřekonatelný problém," řekl v rozhovoru pro iDNES.cz Kalousek. Za velké riziko označil, když banky ztratí chuť poskytovat úvěry.
+        Zdroj: http://ekonomika.idnes.cz/kalousek-posileni-bank-muze-byt-pro-radu-vlad-neprekonatelny-problem-12x-/ekonomika.aspx?c=A111027_115333_ekonomika_kop
+      <p>  		  
+        <p>
+          Dohodu o odmazání poloviny řeckého dluhu nepovažuje ministr financí Miroslav Kalousek za definitivní řešení. Klíčové je podle nich to, zda se podaří posílit evropské banky. "Pro řadu vlád to může být nepřekonatelný problém," řekl v rozhovoru pro iDNES.cz Kalousek. Za velké riziko označil, když banky ztratí chuť poskytovat úvěry.
+          Zdroj: http://ekonomika.idnes.cz/kalousek-posileni-bank-muze-byt-pro-radu-vlad-neprekonatelny-problem-12x-/ekonomika.aspx?c=A111027_115333_ekonomika_kop
+        <p>  		  
+          <p>
+            Dohodu o odmazání poloviny řeckého dluhu nepovažuje ministr financí Miroslav Kalousek za definitivní řešení. Klíčové je podle nich to, zda se podaří posílit evropské banky. "Pro řadu vlád to může být nepřekonatelný problém," řekl v rozhovoru pro iDNES.cz Kalousek. Za velké riziko označil, když banky ztratí chuť poskytovat úvěry.
+            Zdroj: http://ekonomika.idnes.cz/kalousek-posileni-bank-muze-byt-pro-radu-vlad-neprekonatelny-problem-12x-/ekonomika.aspx?c=A111027_115333_ekonomika_kop
+          <p>  		  
+            <p>
+              Dohodu o odmazání poloviny řeckého dluhu nepovažuje ministr financí Miroslav Kalousek za definitivní řešení. Klíčové je podle nich to, zda se podaří posílit evropské banky. "Pro řadu vlád to může být nepřekonatelný problém," řekl v rozhovoru pro iDNES.cz Kalousek. Za velké riziko označil, když banky ztratí chuť poskytovat úvěry.
+              Zdroj: http://ekonomika.idnes.cz/kalousek-posileni-bank-muze-byt-pro-radu-vlad-neprekonatelny-problem-12x-/ekonomika.aspx?c=A111027_115333_ekonomika_kop
+            <p>  		  
+              <p>
+                Dohodu o odmazání poloviny řeckého dluhu nepovažuje ministr financí Miroslav Kalousek za definitivní řešení. Klíčové je podle nich to, zda se podaří posílit evropské banky. "Pro řadu vlád to může být nepřekonatelný problém," řekl v rozhovoru pro iDNES.cz Kalousek. Za velké riziko označil, když banky ztratí chuť poskytovat úvěry.
+                Zdroj: http://ekonomika.idnes.cz/kalousek-posileni-bank-muze-byt-pro-radu-vlad-neprekonatelny-problem-12x-/ekonomika.aspx?c=A111027_115333_ekonomika_kop
+              <p>  		  
+		  
+		  
+		  <div class="obsah"> 
+		    
+
+		    
+		  </div> <!-- end obsah-->
+  		
+		
+  </div>  <!-- konec content  -->
+
+
+
+	<div id="footer">
+	  asdfasdfas
+		<?php
+		  printFooter();
+		?>
+	</div>
+ 
+</body>	
+	
+
+
